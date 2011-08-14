65865aeeadd75ba4a927aeec8942d26dabb4262f
felix@familie-freiberger.net <felix@familie-freiberger.net@76fcad09-962c-2fd6-e9a0-f9cc6dd00c6a>
2011-08-14 12:54:05 +0000

    SVN cleanup

--- trunk/AutoUsers/bin/Debug/BAT_TEMPLATE.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-COLOR 0C
-ECHO Dies ist eine leere Vorlage fuer Batch-Dateien zum Loeschen von Benutzerdaten.
-ECHO Ersetzen Sie diese Datei durch eine auf Ihre Beduerfnisse angepasste Version.
-ECHO.
-ECHO Uebergebener Parameter:
-Echo %1
-PAUSE
